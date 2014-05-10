769f424337b5a0731e2ef1c35e68f8fab4f6a5d1
madorar <lovelive.92048.twinkle0314@gmail.com>
2014-05-10 17:41:04 +0900

    [TDD]ボーリングのスコア計算
    　　　　テストにピンが１つ倒した場合を追加

---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public int Score(int[] pins)
+        {
+            return 1;
+        }
+    }
+}
---
+++ BowlingGame/TestBowlingGame/TestGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BowlingGame;
+
+namespace TestBowlingGame
+{
+    [TestFixture]
+    public class ボウリングのスコア
+    {
+        public void ピンが一つ倒れた場合()
+        {
+            var bowling = new Game();
+            var pins = new int[] { 1 };
+
+            Assert.That(bowling.Score(pins), Is.EqualTo(1));
+
+        }
+    }
+}
